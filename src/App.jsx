4eb02cb5e4599836890833b4d3cc88a95cf8b433
MANU62683 <sibulalbert@gmail.com>
2026-01-26 23:10:32 +0530

    modified by manu

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
        <Navbar />
       
         <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path='/about' element={<AboutPage/>}/>
       </Routes>
