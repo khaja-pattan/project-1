1a5130952cecac6e8e5716455cb4f66cfba0bb21
khaja-pattan <pkhajap768@gmail.com>
2026-01-28 14:47:56 +0530

    page is update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './NavBar/Navbar'
-import Home from './NavBar/Home'
-import { Routes, Route } from 'react-router-dom'   
-import AboutPage from './NavBar/AboutPage'
-import Footer from './Footer/Footer'
-
+import React from 'react'
+import Layout from './KhajaPages/Layout'
 function App() {
- 
-
   return (
-    <>
-       <Navbar />
+    <div>
+      <Layout />
       
-        <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path='/about' element={<AboutPage/>}/>
-      </Routes>
-
-      <Footer/>
-      
-  
-
-    </>
+    </div>
   )
 }
 
